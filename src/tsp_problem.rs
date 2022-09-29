a3e700e5c51e9880f09733770e425c93fe368a1d
yjhn <andrius.puksta@gmail.com>
2022-09-29 14:15:36 +0300

    add city coordinates to TspProblem

--- src/tsp_problem.rs
+++ src/tsp_problem.rs
@@ -14,33 +14,42 @@ const MIN_CITY_COORD: f64 = 0.0;
 const MAX_CITY_COORD: f64 = 1000.0;
 
 pub struct TspProblem {
+    cities: Vec<Point>,
     distances: SquareMatrix<f64>,
-    number_of_cities: usize,
 }
 
 impl TspProblem {
     pub fn random(city_count: usize) -> TspProblem {
-        let distances = generate_cities(city_count);
+        let (cities, distances) = generate_cities(city_count);
 
-        TspProblem {
-            distances,
-            number_of_cities: city_count,
-        }
+        TspProblem { cities, distances }
     }
 
     pub fn from_file(path: impl AsRef<Path>) -> TspProblem {
         let tsp = TspBuilder::parse_path(path).unwrap();
         let number_of_cities = tsp.dim();
+        let cities = {
+            let coord_map = tsp.node_coords();
+            let mut cities = Vec::with_capacity(number_of_cities);
+            for idx in 1..=number_of_cities {
+                let tspf_point_pos = coord_map.get(&idx).unwrap().pos();
+
+                // We only care about 2D.
+                let point = Point::new(tspf_point_pos[0], tspf_point_pos[1]);
+
+                cities.push(point);
+            }
+
+            cities
+        };
+
         let distances = Self::calculate_distances(number_of_cities, &tsp);
 
-        TspProblem {
-            distances,
-            number_of_cities,
-        }
+        TspProblem { cities, distances }
     }
 
     pub fn number_of_cities(&self) -> usize {
-        self.number_of_cities
+        self.cities.len()
     }
 
     pub fn distances(&self) -> &SquareMatrix<f64> {
@@ -64,7 +73,7 @@ impl TspProblem {
 }
 
 // Returns city distance matrix.
-fn generate_cities(city_count: usize) -> SquareMatrix<f64> {
+fn generate_cities(city_count: usize) -> (Vec<Point>, SquareMatrix<f64>) {
     let mut rng = SmallRng::from_entropy();
     let mut cities = Vec::with_capacity(city_count);
 
@@ -85,24 +94,28 @@ fn generate_cities(city_count: usize) -> SquareMatrix<f64> {
         }
     }
 
-    distances
+    (cities, distances)
 }
 
 #[derive(Debug, Clone, Copy)]
-struct Point {
+pub struct Point {
     x: f64,
     y: f64,
 }
 
 impl Point {
-    fn random(rng: &mut impl Rng, range: Range<f64>) -> Point {
+    pub fn new(x: f64, y: f64) -> Point {
+        Point { x, y }
+    }
+
+    pub fn random(rng: &mut impl Rng, range: Range<f64>) -> Point {
         Point {
             x: rng.gen_range(range.clone()),
             y: rng.gen_range(range),
         }
     }
 
-    fn distance(p1: Point, p2: Point) -> f64 {
+    pub fn distance(p1: Point, p2: Point) -> f64 {
         let dx = p2.x - p1.x;
         let dy = p2.y - p1.y;
 
